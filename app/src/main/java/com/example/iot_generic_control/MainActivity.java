b566f224adfd861e80d818763a667da29c6b3195
Carlos Trevisan <xkoeplo@gmail.com>
2021-01-10 15:19:39 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/iot_generic_control/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iot_generic_control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
