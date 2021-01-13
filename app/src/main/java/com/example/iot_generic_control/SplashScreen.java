2c60f9b609332f41a8849cf33de959fbd192e15c
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2021-01-13 11:47:07 -0300

    dev: create and configure splash screen

--- app/src/main/java/com/example/iot_generic_control/SplashScreen.java
+++ app/src/main/java/com/example/iot_generic_control/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.iot_generic_control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashScreen.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
     }
 }
