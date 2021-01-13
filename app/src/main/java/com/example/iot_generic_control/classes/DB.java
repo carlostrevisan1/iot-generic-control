980b7c2384356994449e015664a2b2a64c3a4259
Ian Degaspari <idegaspari@quickium.com>
2021-01-13 20:43:25 -0300

    Banco pronto?

--- app/src/main/java/com/example/iot_generic_control/classes/DB.java
+++ app/src/main/java/com/example/iot_generic_control/classes/DB.java
@@ -1,4 +1,5 @@
 package com.example.iot_generic_control.classes;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -55,8 +56,10 @@ public class DB extends SQLiteOpenHelper{
 
         }
         cursor.close();
+        db.close();
         return devices;
     }
+
     public ArrayList<BaseFeature> selectAllFeatures(){
         SQLiteDatabase db;
         Cursor cursor;
@@ -69,14 +72,119 @@ public class DB extends SQLiteOpenHelper{
             type = cursor.getString(cursor.getColumnIndex("tyoe"));
             switch (type){
                 case "button":
-                    ButtonFeature feat = new ButtonFeature(cursor.getString(cursor.getColumnIndex("name")), )
+                    features.add(new ButtonFeature(cursor.getString(cursor.getColumnIndex("name")),
+                                                   cursor.getString(cursor.getColumnIndex("topic")),
+                                                   cursor.getInt(cursor.getColumnIndex("id")),
+                                                   cursor.getInt(cursor.getColumnIndex("device_id")),
+                                                   cursor.getString(cursor.getColumnIndex("value")), type));
+                    break;
+                case "sendText":
+                    features.add(new SendTextFeature(cursor.getString(cursor.getColumnIndex("name")),
+                                                     cursor.getString(cursor.getColumnIndex("topic")),
+                                                     cursor.getInt(cursor.getColumnIndex("id")),
+                                                     cursor.getInt(cursor.getColumnIndex("device_id")),
+                                                     cursor.getString(cursor.getColumnIndex("value")), type));
+                    break;
+                case "slider":
+                    int startRange, endRange;
+                    String values;
+                    String[] ranges;
+                    values = cursor.getString(cursor.getColumnIndex("value"));
+                    ranges = values.split(";");
+                    startRange = Integer.parseInt(ranges[0]);
+                    endRange = Integer.parseInt(ranges[1]);
+                    features.add(new SliderFeature(cursor.getString(cursor.getColumnIndex("name")),
+                                                   cursor.getString(cursor.getColumnIndex("topic")),
+                                                   cursor.getInt(cursor.getColumnIndex("id")),
+                                                   cursor.getInt(cursor.getColumnIndex("device_id")),
+                                                   startRange, endRange, type));
+                    break;
+                case "toggleButton":
+                    String toggleValues;
+                    String[] onoff;
+                    toggleValues = cursor.getString(cursor.getColumnIndex("value"));
+                    onoff = toggleValues.split(";");
+                    features.add(new ToggleButtonFeature(cursor.getString(cursor.getColumnIndex("name")),
+                                                         cursor.getString(cursor.getColumnIndex("topic")),
+                                                         cursor.getInt(cursor.getColumnIndex("id")),
+                                                         cursor.getInt(cursor.getColumnIndex("device_id")),
+                                                         onoff[0], onoff[1], type));
+                    break;
             }
-            /*devices.add(new IOTDevice(cursor.getString(cursor.getColumnIndex("name")), cursor.getString(cursor.getColumnIndex("desc")),
-                    cursor.getString(cursor.getColumnIndex("ip_address")), cursor.getString(cursor.getColumnIndex("port")),
-                    cursor.getInt(cursor.getColumnIndex("id"))));*/
-
         }
         cursor.close();
+        db.close();
         return features;
+    }
+
+    public long insertDevice(String name, String desc, String ip_address, String port){
+        SQLiteDatabase db;
+        ContentValues values;
+        long res;
+        db = this.getWritableDatabase();
+        values = new ContentValues();
+        values.put("name", name);
+        values.put("desc", desc);
+        values.put("ip_address", ip_address);
+        values.put("port", port);
+        res = db.insert("device", null, values);
+        db.close();
+        return res;
+    }
+
+    public long insertFeature(String name, String topic, String type, String value, int device_id){
+        SQLiteDatabase db;
+        ContentValues values;
+        long res;
+        db = this.getWritableDatabase();
+        values = new ContentValues();
+        values.put("name", name);
+        values.put("topic", topic);
+        values.put("type", type);
+        values.put("value", value);
+        values.put("device_id", device_id);
+        res = db.insert("feature", null, values);
+        db.close();
+        return res;
+    }
+
+    public void updateDevice(int id, String name, String desc, String ip_address, String port){
+        SQLiteDatabase db;
+        ContentValues values;
+        String where;
+        db = this.getWritableDatabase();
+        values = new ContentValues();
+        where = "id = " + id;
+        values.put("name", name);
+        values.put("desc", desc);
+        values.put("ip_address", ip_address);
+        values.put("port", port);
+        db.update("device", values, where, null);
+        db.close();
+    }
+
+    public void updateFeature(int id, String name, String topic, String type, String value, int device_id){
+        SQLiteDatabase db;
+        ContentValues values;
+        String where;
+        db = this.getWritableDatabase();
+        values = new ContentValues();
+        where = "id = " + id;
+        values.put("name", name);
+        values.put("topic", topic);
+        values.put("type", type);
+        values.put("value", value);
+        values.put("device_id", device_id);
+        db.update("feature", values, where, null);
+        db.close();
+    }
+
+    public void deleteFrom(String table, int id){
+        SQLiteDatabase db;
+        String where;
+        where = "id = " + id;
+        db = this.getReadableDatabase();
+        db.delete(table, where, null);
+        db.close();
     }
 }
