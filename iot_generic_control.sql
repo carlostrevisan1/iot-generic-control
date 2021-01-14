0593b699cd949d1cb09298993d5d58243aa34480
Ian Degaspari <idegaspari@quickium.com>
2021-01-14 12:06:43 -0300

    .sql do banco

---
+++ iot_generic_control.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE device (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40),
+    desc TEXT,
+    ip_address VARCHAR(25),
+    port VARCHAR(5)
+);
+
+CREATE TABLE feature (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50),
+    topic VARCHAR(50),
+    type INTEGER,
+    value VARCHAR(100),
+    device_id INTEGER,
+    FOREIGN KEY (device_id) REFERENCES device (id)
+);
