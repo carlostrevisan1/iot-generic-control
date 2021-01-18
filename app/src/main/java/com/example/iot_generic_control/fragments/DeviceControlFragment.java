0ad9abe058424a677fad5426d8763c305e033cca
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2021-01-18 10:48:11 -0300

    refactor: change icon on device control fragment

--- app/src/main/java/com/example/iot_generic_control/fragments/DeviceControlFragment.java
+++ app/src/main/java/com/example/iot_generic_control/fragments/DeviceControlFragment.java
@@ -227,7 +227,7 @@ public class DeviceControlFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.botao_toolbar_inicial, menu);
+        inflater.inflate(R.menu.botao_engrenagem, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
